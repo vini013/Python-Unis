9ce318928c2cb3d38122bbfbec3a3c30d254b18e
Vinicius <vini_59@hotmail.com>
2022-11-05 14:51:29 -0300

    refactor: if Triangle

--- triangle.py
+++ triangle.py
@@ -4,7 +4,7 @@ a = int(input('Coloque o lado A do triangulo: '))
 b = int(input('Coloque o lado B do triangulo: '))
 c = int(input('Coloque o lado C do triangulo: '))
 
-if a > b + c:
+if a > b + c or b > a + c or c > a + b:
     print(str(a) + ' ' + str(b) + ' ' + str(c) + ' nao formam um triangulo')
 else: 
   semiPerimetro = (a + b + c) // 2
