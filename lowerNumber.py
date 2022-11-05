c27a670092c8e92b63d4b6e94f6c55de90d80c22
Vinicius <vini_59@hotmail.com>
2022-11-05 14:30:30 -0300

    feat: Implemented lowerNumber

---
+++ lowerNumber.py
@@ -0,0 +1,9 @@
+numberOne = int(input("Digite o primeiro numero: "))
+numberTwo = int(input("Digite o segundo numero: "))
+numberThree = int(input("Digite o terceiro numero: "))
+
+print(min(numberOne, numberTwo, numberThree))
+
+
+
+  
